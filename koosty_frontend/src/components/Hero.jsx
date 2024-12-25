33983419c877f2f97e25d9a940ca80cae860d0e1
MenassieErmias <ermiasmenase@gmail.com>
2024-12-25 12:16:56 +0300

    hero section done

--- koosty_frontend/src/components/Hero.jsx
+++ koosty_frontend/src/components/Hero.jsx
@@ -1,7 +1,16 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <div className="hero-transluncent absolute w-full -z-10 h-screen bg-black opacity-30">
+
+      </div>
+      <div className="hero-background absolute w-full -z-20 h-screen"></div>
+      <section className='hero-section w-full h-screen pt-20 flex flex-col justify-top items-center gap-[200px]'>
+        <img src="/assets/images/white.png" alt="logo" className="w-[217px]" />
+        <h1 className="hero-heading text-5xl text-center text-gray-50" >Denim <br /> <span className="line-through">Re</span>defined</h1>
+      </section>
+    </>
   )
 }
 
--- koosty_frontend/src/pages/Home.jsx
+++ koosty_frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Hero from '../components/Hero';
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <Hero />
+    </main>
   )
 }
 

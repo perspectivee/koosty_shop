416b30916a7ad4c1d0523c13328def6c9f606c80
MenassieErmias <ermiasmenase@gmail.com>
2024-12-28 10:16:47 +0300

    view more button

--- koosty_frontend/src/components/NewArrivals.jsx
+++ koosty_frontend/src/components/NewArrivals.jsx
@@ -2,11 +2,17 @@ import ProductCard from "./ProductCard"
 
 const NewArrivals = () => {
   return (
-    <section className="w-screen h-[600px]">
+    <section className="w-screen h-[600px] px-10">
       <h2 className="text-gray-900 text-4xl text-center py-10 uppercase">New Arrivals</h2>
-      <div className="card-container">
+      <div className="card-container flex overflow-scroll">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
         <ProductCard />
       </div>
+      <button className="px-8 py-4 border border-blue-600 text-blue-600 text-2xl">
+        View More
+      </button>
     </section>
   )
 }
--- koosty_frontend/src/components/ProductCard.jsx
+++ koosty_frontend/src/components/ProductCard.jsx
@@ -3,11 +3,11 @@ import '../index.css'
 const ProductCard = () => {
   return (
     <div className="product-card">
-      <img className="card-image" src="/assets/images/Group 70.png" alt="card-image" />
+      <img className="card-image" src="/assets/images/image 4.jpg" alt="card-image" />
       <div className="details-container">
         <div className="details">
           <p className="description">
-            Deinim Jacket
+            Denim Jacket
           </p>
           <p className="price">
             2500 ETB

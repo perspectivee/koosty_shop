669b982c6bd17b14e138bd1bc9ca1cd51bf90b62
MenassieErmias <ermiasmenase@gmail.com>
2025-01-10 09:06:34 +0300

    shopping cart icon and font for product card

--- koosty_frontend/src/components/ProductCard.jsx
+++ koosty_frontend/src/components/ProductCard.jsx
@@ -1,11 +1,11 @@
-import { FaCartPlus } from "react-icons/fa";
+import { CiShoppingCart } from "react-icons/ci";
 import '../index.css'
 const ProductCard = () => {
   return (
     <div className="product-card">
       <img className="card-image" src="/assets/images/image 4.jpg" alt="card-image" />
       <div className="details-container">
-        <div className="details">
+        <div className="details  text-gray-600">
           <p className="description">
             Denim Jacket
           </p>
@@ -14,7 +14,7 @@ const ProductCard = () => {
           </p>
         </div>
         <button className="add-to-cart-btn">
-          <FaCartPlus className='add-cart-icon' />
+          <CiShoppingCart className='add-cart-icon' />
         </button>
       </div>
     </div>

1af2819400ba1ea46bd3d45d8af172c9ce198b41
MenassieErmias <ermiasmenase@gmail.com>
2025-01-14 17:42:23 +0300

    hero adjusted to be responsive

--- koosty_tailwind/src/components/Hero.jsx
+++ koosty_tailwind/src/components/Hero.jsx
@@ -3,14 +3,16 @@ import '../index.css';
 const Hero = () => {
   return (
     <section
-      className="h-screen w-full flex flex-col bg-center bg-cover pt-[9.5rem] gap-[6rem] items-center"
+      className="h-screen w-full  bg-center bg-cover "
       style={{ backgroundImage: "url('/assets/images/image 4.jpg')" }}
     >
       <div className='absolute bg-black top-0 left-0 h-screen w-full opacity-20'>
 
       </div>
-      <img className='w-[13.63rem]' src="/assets/images/white.png" alt="logo" />
-      <h1 className="w-[37rem] flex flex-wrap justify-center text-white font-customSerif text-[6.13rem]">Denim&nbsp;<p><span className="line-through">Re</span>Defined</p></h1>
+      <div className="hero-container flex flex-col justify-around p-[2rem] gap-[5rem] items-center">
+        <img className='w-[13.63rem]' src="/assets/images/white.png" alt="logo" />
+        <h1 className="text-center text-white font-customSerif text-[4rem]">Denim&nbsp;<br /><span className="line-through">Re</span>Defined</h1>
+      </div>
     </section>
   )
 }

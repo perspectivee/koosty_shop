7c2dd19da1e40e2c6d03dc977f37b2f9e51d2c28
MenassieErmias <ermiasmenase@gmail.com>
2025-01-14 17:50:16 +0300

    hero hd background

--- koosty_tailwind/src/components/Hero.jsx
+++ koosty_tailwind/src/components/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = () => {
   return (
     <section
       className="h-screen w-full  bg-center bg-cover "
-      style={{ backgroundImage: "url('/assets/images/image 4.jpg')" }}
+      style={{ backgroundImage: "url('/assets/images/hero-bg-hd.webp')" }}
     >
-      <div className='absolute bg-black top-0 left-0 h-screen w-full opacity-20'>
+      {/* <div className='absolute bg-black top-0 left-0 h-screen w-full opacity-20'>
 
-      </div>
+      </div> */}
       <div className="hero-container flex flex-col justify-around p-[2rem] gap-[5rem] items-center">
         <img className='w-[13.63rem]' src="/assets/images/white.png" alt="logo" />
         <h1 className="text-center text-white font-customSerif text-[4rem]">Denim&nbsp;<br /><span className="line-through">Re</span>Defined</h1>

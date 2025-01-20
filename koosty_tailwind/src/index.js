b4d5182449605840b46bb6b1f579a1aebce42733
MenassieErmias <ermiasmenase@gmail.com>
2025-01-20 13:24:09 +0300

    react router dom added as a dependancy and setup, products page created

--- koosty_tailwind/src/index.js
+++ koosty_tailwind/src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './pages/Home';
+import Products from './pages/Products';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

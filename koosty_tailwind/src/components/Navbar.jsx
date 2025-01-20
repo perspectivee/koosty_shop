59429d9c104d2a9abfe6d3d5ec5189599c1fca10
MenassieErmias <ermiasmenase@gmail.com>
2025-01-20 14:38:21 +0300

    Navbar made responsive

--- koosty_tailwind/src/components/Navbar.jsx
+++ koosty_tailwind/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+
+import { CiMenuBurger } from "react-icons/ci";
 const Navbar = () => {
   return (
     <nav className="flex p-8 justify-around">
-      <img className="h-[5rem]" src="/assets/images/black.png" alt="logo" />
-      <ul className="w-[40%] flex justify-start h-[5rem] items-center">
+      <img className="h-[3rem] md:h-[5rem]" src="/assets/images/black.png" alt="logo" />
+      <ul className="w-[40%] hidden md:flex justify-start h-[3rem] md:h-[5rem] items-center">
         <li className="mr-[4rem]">
           <Link className="font-customSerif capitalize text-[1.9rem]" to='/'>
             Home
@@ -20,6 +22,7 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+      <CiMenuBurger className="md:hidden block text-[3rem]" />
     </nav>
   )
 }

7cf4cd29c87e71d6b45441b6222492e10596bc8d
MenassieErmias <ermiasmenase@gmail.com>
2025-01-20 12:39:32 +0300

    word break class added to popular picks

--- koosty_tailwind/src/components/PopularPicksButton.jsx
+++ koosty_tailwind/src/components/PopularPicksButton.jsx
@@ -2,12 +2,14 @@
 
 const PopularPicksButton = ({ imgURL, name }) => {
   return (
-    <button className="text-white font-customSans flex justify-center items-center flex-1 border text-[3rem] uppercase bg-center bg-cover"
+    <button className="text-white font-customSans flex justify-center items-center basis-1/3 border text-[3rem] uppercase bg-center bg-cover break-words"
       style={{
         backgroundImage: `url(${imgURL})`
       }}
     >
-      {name}
+      <p className="max-w-[90%] break-words">
+        {name}
+      </p>
     </button>
   )
 }

cc52b74aba8cc67e8012a4788f775f86df56934b
MenassieErmias <ermiasmenase@gmail.com>
2025-01-21 18:55:24 +0300

    ProductDetail page made responsive for large devices

--- koosty_tailwind/src/pages/ProductDetail.jsx
+++ koosty_tailwind/src/pages/ProductDetail.jsx
@@ -18,27 +18,29 @@ const ProductDetail = () => {
   return (
     <main>
       <Navbar />
-      <section className="h-max p-8 flex flex-col md:flex-row items-center">
-        <img className="h-[40rem] object-cover" src={data.mainImgURL} alt="main image" />
-        <div className="flex w-[90%] justify-center mt-[1rem]">
-          <img className="w-[33%] object-cover" src={data.imgURL1} alt="image 1" />
-          <img className="w-[33%] object-cover" src={data.imgURL2} alt="image 2" />
-          <img className="w-[33%] object-cover" src={data.imgURL3} alt="image 3" />
+      <section className="h-max p-8 flex flex-col items-center lg:flex-row lg:items-start lg:gap-4">
+        <img className="h-[30rem] object-cover lg:order-2 lg:h-[]" src={data.mainImgURL} alt="main image" />
+        <div className="flex w-[90%] justify-center mt-[1rem] lg:order-1 lg:flex-col lg:w-[400px] lg:mt-0 lg:items-start lg:gap-4">
+          <img className="w-[33%] lg:w-full lg:h-[9rem] object-cover" src={data.imgURL1} alt="image 1" />
+          <img className="w-[33%] lg:w-full lg:h-[9rem] object-cover" src={data.imgURL2} alt="image 2" />
+          <img className="w-[33%] lg:w-full lg:h-[9rem] object-cover" src={data.imgURL3} alt="image 3" />
         </div>
-        <h2 className="font-customSans text-[2rem] tracking-widest mt-[1rem]">
-          {data.name}
-        </h2>
-        <p className="font-customSerif text-gray-700">
-          {data.detail}
-        </p>
-        <p className="font-customSans text-[1.7rem] text-gray-700 mt-4">
-          {data.price}
-        </p>
-        <button className="w-[70%]  flex justify-center py-4 px-8 mt-4"
-          style={{ background: 'linear-gradient(0deg, rgba(1, 69, 103, 0.47) -15.07%, rgba(2, 137, 205, 0.2585) 155.36%)' }}
-        >
-          <BsCartFill className="text-white text-[2.2rem]" />
-        </button>
+        <div className="lg:order-3">
+          <h2 className="font-customSans text-[2rem] tracking-widest mt-[1rem]">
+            {data.name}
+          </h2>
+          <p className="font-customSerif text-gray-700">
+            {data.detail}
+          </p>
+          <p className="font-customSans text-[1.7rem] text-gray-700 mt-4">
+            {data.price}
+          </p>
+          <button className="w-[70%]  flex justify-center py-4 px-8 mt-4"
+            style={{ background: 'linear-gradient(0deg, rgba(1, 69, 103, 0.47) -15.07%, rgba(2, 137, 205, 0.2585) 155.36%)' }}
+          >
+            <BsCartFill className="text-white text-[2.2rem]" />
+          </button>
+        </div>
       </section>
       <Footer />
     </main>
